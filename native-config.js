4f1eac54911d5a276bf7ea97ec9cdd251ee05ec4
Akilesh.T <akilesht@karunya.edu.in>
2026-09-21 12:27:53 +0530

    Delete native-config.js

--- native-config.js
+++
@@ -1,2 +0,0 @@
-// Native Capacitor routing. Set SYNTH_PUBLIC_SERVER_URL at build time or change synthServerUrl locally.
-window.SYNTH_SERVER_URL = window.SYNTH_PUBLIC_SERVER_URL || localStorage.getItem('synthServerUrl') || 'http://10.90.82.78:3000';
